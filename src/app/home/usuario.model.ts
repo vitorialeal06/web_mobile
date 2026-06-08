01df65b113c8a6cb007ffbabbdbfb2dae5f6bfcd
Vitoria <vitorialealsantos06@gmail.com>
2026-06-08 10:21:11 -0300

    sitema-mobile finalizado

--- src/app/home/usuario.model.ts
+++ src/app/home/usuario.model.ts
@@ -1,9 +1,13 @@
-/*
-export class Usuario
-{
+export class Usuario {
   public id: number;
   public nome: string;
   public email: string;
- public token: string;  
+  public token: string;
+
+  constructor() {
+    this.id = 0;
+    this.nome = '';
+    this.email = '';
+    this.token = '';
+  }
 }
-*/
